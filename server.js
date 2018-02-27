adfad246e214e314e9d240b63742975ca057adb0
Peter McAree <p.mcaree@kainos.com>
2018-02-27 23:14:59 +0000

    Updating npm scripts, adding Procfile and adding Knex endpoints

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const env = process.env.NODE_ENV || 'development';
+const knexConfig = require('./knexfile');
+const knex = require('knex')(knexConfig[env]);
+
 const app = express();
 app.use(bodyParser.json());
 
 // Serve static content from /dist folder (populated after ng build command)
 app.use(express.static(__dirname + '/dist'));
 
-/*
--- Uncomment this section if using Knex! --
-
-const env = process.env.NODE_ENV || 'development';
-const knexConfig = require('./knexfile');
-const knex = require('knex')(knexConfig[env]);
-
 app.get('/languages', (req, res) => {
     return knex('languages')
         .then((result) => res.send(result));
 });
-*/
 
 app.listen(process.env.PORT || 8080, () => console.log('Server listening on 8080'));
